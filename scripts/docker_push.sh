a41707d06c0afdcd4076fa087004a32774e3b2c9
Keith Wall <kwall@apache.org>
2018-05-25 22:20:19 +0100

    Patch for MacOSX

--- scripts/docker_push.sh
+++ scripts/docker_push.sh
@@ -1,6 +1,21 @@
 #!/usr/bin/env bash
 
-curdir="$(dirname $(readlink -f ${0}))"
+# resolve links - $0 may be a softlink
+PRG="$0"
+
+while [ -h "$PRG" ]; do
+  ls=`ls -ld "$PRG"`
+  link=`expr "$ls" : '.*-> \(.*\)$'`
+  if expr "$link" : '/.*' > /dev/null; then
+    PRG="$link"
+  else
+    PRG=`dirname "$PRG"`/"$link"
+  fi
+done
+curdir="$(dirname $PRG)"
+
+
+
 source "${curdir}/logger.sh"
 
 if [[ -f Dockerfile ]]; then
