fa172eaa165e574c3e0698ebadaa3e983131b9fe
Keith Wall <kwall@apache.org>
2019-03-18 15:39:00 +0000

    Use oauth-proxy to serve content (replacing go) and orchestrate oauth2 login against openshift (or kubernettes)

--- console/ui/src/App/index.js
+++ console/ui/src/App/index.js
@@ -1,18 +1,9 @@
 import React, {Component} from 'react';
 import './app.css';
 import Layout from "../components/Layout/Layout"
-import OAuth2User from "../components/OAuth2User";
-import UnauthenticatedUser from "../components/UnauthenticatedUser";
+import User from "../components/User";
 
-let oauth = false;
-
-// TODO need to inject the oauth endpoints etc from the server side.
-const user = oauth ? (<OAuth2User authorizationEndpoint='https://accounts.google.com/o/oauth2/v2/auth'
-                                  userInfoEndpoint='https://openidconnect.googleapis.com/v1/userinfo'
-                                  scope='openid profile'
-                                  redirectUri='http://localhost:9000'
-                                  clientId='1045256966207-mie784ka26ncjb1gvd98jatldd0ct42v.apps.googleusercontent.com'/>)
-  : <UnauthenticatedUser/>;
+const user = <User/>;
 
 export default class App extends Component {
 
